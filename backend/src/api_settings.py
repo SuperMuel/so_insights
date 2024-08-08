69b043358306b3faddfd6caf1c938262cd6c7176
Samuel Mallet <69467005+SuperMuel@users.noreply.github.com>
2024-08-08 11:08:48 +0200

    allow undefined mongodb_uri on CI

--- backend/src/api_settings.py
+++ backend/src/api_settings.py
@@ -1,5 +1,6 @@
 # base settings using pydantic-settings to define the MONGODB_URI and PORT variables from env
 
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
@@ -11,5 +12,9 @@ class APISettings(BaseSettings):
         env_file_encoding="utf-8",
         extra="ignore",
     )
-    MONGODB_URI: str = Field(default=...)
+    MONGODB_URI: str = (
+        Field(  # If generating client sdk on github_action, ignore the uri
+            default="mongodb://localhost:27017" if os.getenv("GITHUB_ACTIONS") else ...
+        )
+    )
     PORT: int = Field(default=8000)
