31243124aec01638ee1dec45131059e471d03868
Samuel Mallet <69467005+SuperMuel@users.noreply.github.com>
2024-08-12 13:46:02 +0200

    1st refactor

--- ingester/main.py
+++ ingester/main.py
@@ -18,6 +18,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+settings = IngesterSettings()
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -82,9 +84,139 @@ async def mark_articles_as_vector_indexed(articles: list[Article]):
     logger.info("Marked as vector indexed.")
 
 
-async def main():
-    settings = IngesterSettings()
+async def handle_search_query_set(
+    search_query_set: SearchQuerySet,
+    ddgs: AsyncDDGS,
+    get_pinecone_index,
+):
+    assert search_query_set.id
 
+    logger.info(
+        f"Processing search query set {search_query_set.id} ({search_query_set.title}, {len(search_query_set.queries)} queries, timelimit='{settings.TIME_LIMIT}')"
+    )
+
+    if not search_query_set.queries:
+        logger.info(
+            f"Skipping search query set {search_query_set.id} because it has no queries"
+        )
+        return
+
+    last_run = await search_query_set.find_last_finished_run()
+
+    if last_run is not None:
+        if last_run.is_running():
+            logger.warning(
+                f"Skipping search query set {search_query_set.id} because the last run is not finished"
+            )
+            return
+        if has_run_less_than_24h_ago(last_run):
+            logger.info(
+                f"Skipping search query set {search_query_set.id} because the last run was less than 24h ago"
+            )
+            return
+
+    # Search can begin
+    run: IngestionRun = await IngestionRun(
+        workspace_id=search_query_set.workspace_id,
+        queries_set_id=search_query_set.id,
+        status="running",
+        time_limit=settings.TIME_LIMIT,
+        max_results=settings.MAX_RESULTS,
+    ).insert()
+
+    try:
+        result = await perform_search(
+            ddgs,
+            queries=search_query_set.queries,
+            region=search_query_set.region,
+            max_results=run.max_results,
+            time_limit=run.time_limit,
+            verbose=settings.VERBOSE_SEARCH,
+        )
+    except Exception as e:
+        logger.error(
+            f"Error while processing search query set {search_query_set.id}: {e}"
+        )
+        run.status = "failed"
+        run.error = str(e)
+        await run.replace()
+        return
+
+    run.successfull_queries = result.successfull_queries
+    logger.info(
+        f"{result.successfull_queries}/{len(search_query_set.queries)} queries were successful. Found {len(result.articles)} (undeduplicated) articles"
+    )
+
+    articles = deduplicate_articles(result.articles)
+
+    logger.info(f"Deduplicated to {len(articles)} articles")
+
+    n_inserted = None
+
+    if not articles:
+        logger.info("No articles to upsert")
+        n_inserted = 0
+    else:
+        logger.info("Upserting articles to mongodb")
+        try:
+            inserted = await Article.insert_many(
+                [
+                    Article(
+                        workspace_id=search_query_set.workspace_id,
+                        region=search_query_set.region,
+                        **article.model_dump(),
+                    )
+                    for article in articles
+                ],
+                ordered=False,
+            )
+            logger.info(f"Inserted {len(inserted.inserted_ids)} articles to mongodb")
+            n_inserted = len(inserted.inserted_ids)
+        except BulkWriteError as e:
+            logger.info(
+                f"Inserted {e.details['nInserted']} new documents into MongoDB."
+            )
+            logger.info(f"Encountered {len(e.details['writeErrors'])} duplicates.")
+            n_inserted = e.details["nInserted"]
+
+        try:
+            pinecone_index = get_pinecone_index(str(search_query_set.workspace_id))
+            indexed = await index_not_indexed_articles(pinecone_index)
+        except Exception as e:
+            logger.error(
+                f"Error while indexing articles: {e}. In mongoDB, we won't mark them as indexed. You'll have to manually check the synchronization between the vector store and mongodb."
+            )
+        else:
+            await mark_articles_as_vector_indexed(indexed)
+
+    assert n_inserted is not None
+    run.n_inserted = n_inserted
+    run.status = "completed"
+    run.end_at = datetime.now(timezone.utc)
+    await run.replace()
+
+
+async def handle_all_search_query_sets(ddgs: AsyncDDGS, get_pinecone_index):
+    search_query_sets = await SearchQuerySet.find_all().to_list()
+
+    logger.info(f"Processing {len(search_query_sets)} search query sets")
+
+    for i, search_query_set in enumerate(search_query_sets):
+        logger.info(
+            f"Processing search query set {search_query_set.title} ({search_query_set.id}) ({i + 1}/{len(search_query_sets)})"
+        )
+        await handle_search_query_set(
+            search_query_set=search_query_set,
+            ddgs=ddgs,
+            get_pinecone_index=get_pinecone_index,
+        )
+
+        logger.info(f"Finished processing search query set {search_query_set.id}")
+
+    logger.info("Finished processing all search query sets")
+
+
+async def main():
     mongo_client = get_client(settings.MONGODB_URI)
     await my_init_beanie(mongo_client)
 
@@ -104,124 +236,8 @@ async def main():
 
     ddgs = AsyncDDGS(timeout=settings.QUERY_TIMEOUT)
 
-    # TODO : do not get all SearchQuerySets, but implement a ScheduledSearch class that will be responsible for scheduling the search queries, and can be disabled at any time
-    # TODO : this only works if there is a single worker.
-    search_query_sets = await SearchQuerySet.find_all().to_list()
+    await handle_all_search_query_sets(ddgs, get_pinecone_index)
 
-    logger.info(f"Processing {len(search_query_sets)} search query sets")
-
-    for i, search_query_set in enumerate(search_query_sets):
-        assert search_query_set.id
-
-        logger.info(
-            f"Processing search query set {search_query_set.title} ({search_query_set.id}) ({i + 1}/{len(search_query_sets)})"
-        )
-
-        if not search_query_set.queries:
-            logger.info(
-                f"Skipping search query set {search_query_set.id} because it has no queries"
-            )
-            continue
-
-        last_run = await search_query_set.find_last_finished_run()
-
-        if last_run is not None:
-            if last_run.is_running():
-                logger.warning(
-                    f"Skipping search query set {search_query_set.id} because the last run is not finished"
-                )
-                continue
-            if has_run_less_than_24h_ago(last_run):
-                logger.info(
-                    f"Skipping search query set {search_query_set.id} because the last run was less than 24h ago"
-                )
-                continue
-
-        # Search can begin
-        run: IngestionRun = await IngestionRun(
-            workspace_id=search_query_set.workspace_id,
-            queries_set_id=search_query_set.id,
-            status="running",
-            time_limit=settings.TIME_LIMIT,
-            max_results=settings.MAX_RESULTS,
-        ).insert()
-
-        try:
-            result = await perform_search(
-                ddgs,
-                queries=search_query_set.queries,
-                region=search_query_set.region,
-                max_results=run.max_results,
-                time_limit=run.time_limit,
-                verbose=settings.VERBOSE_SEARCH,
-            )
-        except Exception as e:
-            logger.error(
-                f"Error while processing search query set {search_query_set.id}: {e}"
-            )
-            run.status = "failed"
-            run.error = str(e)
-            await run.replace()
-            continue
-
-        run.successfull_queries = result.successfull_queries
-        logger.info(
-            f"{result.successfull_queries}/{len(search_query_set.queries)} queries were successful. Found {len(result.articles)} (undeduplicated) articles"
-        )
-
-        articles = deduplicate_articles(result.articles)
-
-        logger.info(f"Deduplicated to {len(articles)} articles")
-
-        n_inserted = None
-
-        if not articles:
-            logger.info("No articles to upsert")
-            n_inserted = 0
-        else:
-            logger.info("Upserting articles to mongodb")
-            try:
-                inserted = await Article.insert_many(
-                    [
-                        Article(
-                            workspace_id=search_query_set.workspace_id,
-                            region=search_query_set.region,
-                            **article.model_dump(),
-                        )
-                        for article in articles
-                    ],
-                    ordered=False,
-                )
-                logger.info(
-                    f"Inserted {len(inserted.inserted_ids)} articles to mongodb"
-                )
-                n_inserted = len(inserted.inserted_ids)
-            except BulkWriteError as e:
-                logger.info(
-                    f"Inserted {e.details['nInserted']} new documents into MongoDB."
-                )
-                logger.info(f"Encountered {len(e.details['writeErrors'])} duplicates.")
-                n_inserted = e.details["nInserted"]
-
-            try:
-                pinecone_index = get_pinecone_index(str(search_query_set.workspace_id))
-                indexed = await index_not_indexed_articles(pinecone_index)
-            except Exception as e:
-                logger.error(
-                    f"Error while indexing articles: {e}. In mongoDB, we won't mark them as indexed. You'll have to manually check the synchronization between the vector store and mongodb."
-                )
-            else:
-                await mark_articles_as_vector_indexed(indexed)
-
-        assert n_inserted is not None
-        run.n_inserted = n_inserted
-        run.status = "completed"
-        run.end_at = datetime.now(timezone.utc)
-        await run.replace()
-
-        logger.info(f"Finished processing search query set {search_query_set.id}")
-
-    logger.info("Finished processing all search query sets")
     mongo_client.close()
 
 
