d203b807b481335e4d0ff76d1241c8bfaefa7841
Samuel Mallet <69467005+SuperMuel@users.noreply.github.com>
2024-08-13 14:27:00 +0200

    Analysis -> Topcis

--- frontend/app.py
+++ frontend/app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 
-
 if __name__ == "__main__":
     pg = st.navigation(
         [
             st.Page("pages/1_🗂️Workspaces.py"),
             st.Page("pages/2_📥Ingestion.py"),
             st.Page("pages/3_💬Chatbot.py"),
-            st.Page("pages/4_🔍Analysis.py"),
+            st.Page("pages/4_🔍Topics.py"),
         ]
     )
-    
+
     from src.shared import show_all_toasts
+
     show_all_toasts()
     pg.run()
